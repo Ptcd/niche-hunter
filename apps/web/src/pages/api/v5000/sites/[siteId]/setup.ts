e29c955dc18913db94310bac4ab678f86f9b05cf
Colin Merrill <merrillholdings@gmail.com>
2025-11-24 11:08:33 -0600

    Fix: include niche relation in site query

--- apps/web/src/pages/api/v5000/sites/[siteId]/setup.ts
+++ apps/web/src/pages/api/v5000/sites/[siteId]/setup.ts
@@ -42,6 +42,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       where: { id: siteId },
       include: {
         batch: true,
+        niche: true,
       },
     });
 
